22afc1fc7c2e9c1468f7fc378d04fbda6f6d7c49
GodhaRG <96509418+GodhaRG@users.noreply.github.com>
2022-02-03 09:17:50 +0000

    sum of n numbers

---
+++ p3final.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input_n()
+{
+  printf("enter the number\n");
+  int x;
+  scanf("%d",&x);
+  return x;
+}
+int sum_n(int n)
+{
+  int r=0;
+  r=(n*(n+1))/2;
+  return r;
+}
+void output(int n,int sum)
+{
+  printf("1");
+  for(int f=2;f<n;f++)
+  {
+   printf("+%d",f);
+  }
+  printf("+%d is %d\n",n,sum);
+}
+int main()
+{
+  int n ,sm;
+  n=input_n();
+  sm=sum_n(n);
+  output(n,sm);
+  return 0;
+}
--- p3original.c
+++ p3original.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int input_n()
 {
+  printf("enter the number\n");
   int x;
   scanf("%d",&x);
   return x;
